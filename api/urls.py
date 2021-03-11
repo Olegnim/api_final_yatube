acef0ecd6419199000e0855e8373b1f5eaa03048
Oleg Sokolov <olegnim@gmail.com>
2021-03-11 21:06:35 +0300

    Beta version

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,8 @@
 from django.urls import include, path
+from django.views.generic import TemplateView
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (TokenObtainPairView,
+                                            TokenRefreshView)
 
 from .views import (CommentViewSet, FollowViewSet, GroupViewSet, PostViewSet,
                     UserViewSet)
@@ -18,4 +21,19 @@ router_v1.register(r'group', GroupViewSet, basename='group')
 
 urlpatterns = [
     path('v1/', include(router_v1.urls)),
+    path(
+        'v1/token/',
+        TokenObtainPairView.as_view(),
+        name='token_obtain_pair'
+    ),
+    path(
+        'v1/token/refresh/',
+        TokenRefreshView.as_view(),
+        name='token_refresh'
+    ),
+    path(
+        'redoc/',
+        TemplateView.as_view(template_name='redoc.html'),
+        name='redoc'
+    ),
 ]
--- api/views.py
+++ api/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import get_object_or_404
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, status, viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
@@ -21,17 +22,11 @@ class UserViewSet(viewsets.ViewSet):
 
 
 class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = PERMISSION_CLASSES
-    filter_backends = [filters.SearchFilter]
-    search_fields = ['group', ]
-
-    def get_queryset(self):
-        queryset = Post.objects.all()
-        group = self.request.query_params.get('group', None)
-        if group is not None:
-            queryset = queryset.filter(group=group)
-        return queryset
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['group', ]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
